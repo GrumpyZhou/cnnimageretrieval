2b06c60009e9cb030f2c2db44f4f014f70a9d207
Qunjie Zhou <jennyzqj00@gmail.com>
2018-02-09 22:15:10 +0800

    Fix ..dir bug

--- examples/7scenes/generate_train_knn_mat.m
+++ examples/7scenes/generate_train_knn_mat.m
@@ -16,14 +16,14 @@ Lw = whiten.Lw;
 % Calculate similarity matrix for training pairs
 %---------------------------------------------------------------------
 % Set path to store the result
-result_dir = fullfile(data_root, '7scenes');
+base_dir = fullfile(data_root, '7scenes');
+result_dir = fullfile(base_dir, 'train');
 if ~exist(result_dir, 'dir')
     mkdir(result_dir);
-    mkdir(fullfile(result_dir, 'train'));
 end
 for d = 1:numel(test_datasets)
     dataset = test_datasets{d};
-    desc_file = fullfile(result_dir, 'vgg-mac-1024-desc', sprintf('%s.mat', dataset));
+    desc_file = fullfile(base_dir, 'vgg-mac-1024-desc', sprintf('%s.mat', dataset));
     fprintf('>> %s: Load pre-saved descriptors for training and query/test images...\n', dataset); 
     res = load(desc_file);
     vecs = res.vecs;
@@ -35,7 +35,7 @@ for d = 1:numel(test_datasets)
     fprintf('>> %s: K-NearestNeighbour Retrieval...\n', dataset);
     sim = vecsLw'*qvecsLw;
     [sim, ranks] = sort(sim, 'descend');
-    idx_file = fullfile(result_dir, 'train', sprintf('%s-knn.mat', dataset));
+    idx_file = fullfile(result_dir, sprintf('%s-knn.mat', dataset));
     save(idx_file, 'sim');
     save(idx_file, 'ranks', '-append');
     fprintf('>> Save knn result to %s', idx_file);
